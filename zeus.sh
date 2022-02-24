6a6194a64dd4cb83b2f498bbb36d4ea4966abe23
Pablo Baeyens <pbaeyens31+github@gmail.com>
2022-02-24 18:55:02 +0100

    Script para ejecutar solución

---
+++ zeus.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+pushd inputs
+time python3 ../solution.py a_an_example.in.txt
+time python3 ../solution.py b_better_start_small.in.txt
+time python3 ../solution.py c_collaboration.in.txt
+time python3 ../solution.py d_dense_schedule.in.txt
+time python3 ../solution.py e_exceptional_skills.in.txt
+time python3 ../solution.py f_find_great_mentors.in.txt
+popd
+
+zip ohno -r *.py
