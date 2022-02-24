4c87967cee688ca503b3cb2bbce44245cad7ff08
Pablo Baeyens <pbaeyens31+github@gmail.com>
2022-02-24 19:08:11 +0100

    Data structures

--- parse.py
+++ parse.py
@@ -2,14 +2,35 @@ from dataclasses import dataclass
 from collections import defaultdict
 from typing import Dict, List, Any
 
+@dataclass
+class Role:
+    language: str
+    level: int
 
+@dataclass
+class Contributor:
+    name: str
+    N: int
+    skills: List[Role]
+
+@dataclass
+class Project:
+    name: str
+    duration: int
+    score: int
+    best_before: int
+    R: int
+    roles: List[Role]
 
 @dataclass
 class Input:
-    pass
+    C: int
+    P: int
+    contributors: List[Contributor]
+    projects: List[Project]
 
 
 def read_file(_: str) -> Input:
-    return Input()   
+    return Input(0,0,[],[])   
     
 
