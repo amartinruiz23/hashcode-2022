c77a62743cb3e0c5ae94efb09177c1db066d71c5
Pablo Baeyens <pbaeyens31+github@gmail.com>
2022-02-24 19:22:15 +0100

    Add parser

--- parse.py
+++ parse.py
@@ -30,7 +30,44 @@ class Input:
     projects: List[Project]
 
 
-def read_file(_: str) -> Input:
-    return Input(0,0,[],[])   
+def read_file(name: str) -> Input:
+    with open(name, 'r') as f:
+        C, P = [int(x) for x in f.readline().split(" ")]
+
+        contributors = []
+        for _ in range(C):
+            name_and_N = f.readline().split(" ")
+            contributor_name = name_and_N[0]
+            N = int(name_and_N[1])
+            skills = []
+
+            for _ in range(N):
+                raw_skill = f.readline().split(" ")
+                skill_name = raw_skill[0]
+                level = int(raw_skill[1])
+                skills.append(Role(skill_name, level))
+            
+            contributors.append(Contributor(contributor_name, N, skills))
+        
+        projects = []
+        for _ in range(P):
+            name_and_stuff = f.readline().split(" ")
+            project_name = name_and_stuff[0]
+            duration = int(name_and_stuff[1])
+            score = int(name_and_stuff[2])
+            best_before = int(name_and_stuff[3])
+            R = int(name_and_stuff[4])
+
+            roles = []
+            for _ in range(R):
+                role_and_level = f.readline().split()
+                role_name = role_and_level[0]
+                role_level = int(role_and_level[1])
+                roles.append(Role(role_name, role_level))
+            
+            projects.append(Project(project_name, duration, score, best_before, R, roles))
+
+
+    return Input(C,P,contributors,projects)   
     
 
--- solution.py
+++ solution.py
@@ -12,9 +12,9 @@ class Solution:
     def __init__(self):
         pass
 
-    def write_to_file(self, path: str, inp):
+    def write_to_file(self, path: str, inp: parse.Input):
         with open(path, 'w') as f:
-            print("This is the output", file=f)
+            print(inp, file=f)
 
 inp = parse.read_file(sys.argv[1])
 solution = Solution()
