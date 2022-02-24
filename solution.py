fb56a3b915fa46d5401f63ebf59c96fbf05aab9f
Pablo Baeyens <pbaeyens31+github@gmail.com>
2022-02-24 19:43:22 +0100

    Salida

--- solution.py
+++ solution.py
@@ -2,20 +2,35 @@
 import parse
 import sys
 from dataclasses import dataclass
-from typing import Dict
+from typing import Dict, List
 
-Intersection = int
-Street = str
+
+@dataclass
+class Assignment:
+    name: str
+    assignees: List[str]
 
 @dataclass
 class Solution:
-    def __init__(self):
-        pass
+    projects : List[Assignment]
+
+    def __init__(self, projs):
+        self.projects = projs
 
     def write_to_file(self, path: str, inp: parse.Input):
         with open(path, 'w') as f:
-            print(inp, file=f)
+            print(len(self.projects), file=f)
+
+            for assignment in self.projects:
+                print(assignment.name, file=f)
+                print(" ".join(assignment.assignees), file=f)
+
+
 
 inp = parse.read_file(sys.argv[1])
-solution = Solution()
+solution = Solution([
+    Assignment("WebServer", ["Bob", "Anna"]),
+    Assignment("Logging", ["Anna"]),
+    Assignment("WebChat", ["Maria", "Bob"]),
+])
 solution.write_to_file('../outputs/{}'.format(sys.argv[1]), inp)
